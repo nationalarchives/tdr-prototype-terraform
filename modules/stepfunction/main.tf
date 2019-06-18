c1b6058b4dbe3d04d4f381eef98166c15cedf228
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-06-18 07:49:04 +0100

    Add test step function

--- modules/stepfunction/main.tf
+++ modules/stepfunction/main.tf
@@ -1,7 +1,19 @@
 resource "aws_sfn_state_machine" "sfn_state_machine" {
   name = var.step_function_name
   role_arn = "${aws_iam_role.iam_for_sfn.arn}" //"aws:iam::247222723249:role/service-role/TestS3Role"
-  definition = <<EOF ""
+  definition = <<EOF
+    {
+      Comment": "A Hello World example of the Amazon States Language using an AWS Lambda Function",
+      "StartAt": "HelloWorld",
+      "States": {
+        "HelloWorld": {
+          "Type": "Task",
+          "Resource": "${aws_lambda_function.lambda.arn}",
+          "End": true
+        }
+      }
+    }
+    EOF
 
   tag = {
     Name = var.tag_name
