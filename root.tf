c98afdfc3f4059adc162ba4f160a8c3429263890
TomJKing <Tom.King@nationalarchives.gov.uk>
2019-06-24 07:45:06 +0100

    Add global variables and additional modules to root

--- root.tf
+++ root.tf
@@ -1,5 +1,8 @@
 locals {
+  #Ensure that developers' workspaces always default to 'dev'
   environment = lookup(var.workspace_to_environment_map, terraform.workspace, "dev")
+  tag_prefix  = module.global_variables.tag_prefix
+  aws_region  = module.global_variables.default_aws_region
 }
 
 terraform {
@@ -13,14 +16,33 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = local.aws_region
+}
+
+module "global_variables" {
+  source = "./modules/global_variables"
 }
 
 module "cognito" {
-  #Source will be the module github repository
   source = "./modules/cognito"
 
-  cognito_user_pool = "${var.tag_prefix}-${local.environment}"
-  tag_name          = "${var.tag_prefix}-authentication-${local.environment}-userpool1"
+  cognito_user_pool = "${local.tag_prefix}-${local.environment}"
+  tag_name          = "${local.tag_prefix}-authentication-${local.environment}-userpool1"
   environment       = local.environment
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+
+  vpc_name = "${local.tag_prefix}-${local.environment}-vpc1"
+  cidr     = "10.0.0.0/17"
+  tag_name = "${local.tag_prefix}-cloud-${local.environment}-vpc1"
+}
+
+module "stepfunction" {
+  source = "./modules/stepfunction"
+
+  step_function_name = "${local.tag_prefix}-${local.environment}-stepfunc1"
+  tag_name           = "${local.tag_prefix}-stepfunc-${local.environment}-sf"
+  vpc_id             = module.vpc.vpc_id
 }
--- root_variables.tf
+++ root_variables.tf
@@ -1,21 +1,10 @@
 variable "workspace_to_environment_map" {
   type = "map"
 
+  //Maps the Terraform workspace to the AWS environment.
   default = {
     dev  = "dev"
     test = "test"
     prod = "prod"
   }
-}
-
-variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "eu-west-2"
-}
-
-variable "tag_prefix" {
-  description = "Prefix for module tags to identify the project the module belongs to"
-  type        = string
-  default     = "tdr-prototype"
 }
