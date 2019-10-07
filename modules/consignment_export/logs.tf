d144bf8656529a0079a60bf6f38ba83c59629182
Suzanne Hamilton <suzanne.hamilton@nationalarchives.gov.uk>
2019-10-07 16:31:33 +0100

    Fix name of consignment export log resource
    The old name was copy-pasted from the DB migration task.

--- modules/consignment_export/logs.tf
+++ modules/consignment_export/logs.tf
@@ -1,4 +1,4 @@
-resource "aws_cloudwatch_log_group" "database_migration_task" {
+resource "aws_cloudwatch_log_group" "consignment_export_task" {
   name              = local.export_task_log_group_name
   retention_in_days = 30
 }
